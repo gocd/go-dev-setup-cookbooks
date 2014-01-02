ffd75290c3ab170b020cc902b3dc7be01414cb32
Aravind SV <arvind.sv@gmail.com>
2014-01-02 15:28:00 +0530

    Added autologin for console.

---
+++ cookbooks/startup-setup-console/recipes/default.rb
@@ -0,0 +1,11 @@
+#setting up auto login for console
+
+script "startup-setup-console" do
+  interpreter "bash"
+  user "root"
+  cwd "/home/vagrant"
+  code <<-EOH
+    sed -e 's#exec /sbin/getty#& --autologin vagrant#' /etc/init/tty1.conf
+  EOH
+end
+
