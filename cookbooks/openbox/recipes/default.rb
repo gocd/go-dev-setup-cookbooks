8c8b893ef4f99bd4c067cfde2df475769dd17642
Sharanya Bathey <sharanyabathey@gmail.com>
2014-03-17 11:33:08 +0530

    Adding a dock & menu item for IntelliJ
    Updating cookbooks.tar.gz to include Dock & Menu item for IntelliJ

--- cookbooks/openbox/recipes/default.rb
+++ cookbooks/openbox/recipes/default.rb
@@ -16,11 +16,26 @@ cookbook_file "/home/vagrant/.bash_profile" do
   mode "0664"
 end
 
+cookbook_file "/home/vagrant/menu.xml" do
+  owner "vagrant"
+  group "vagrant"
+  source "menu.xml"
+  backup false
+  mode "0664"
+end
+
 script "install_openbox" do
   interpreter "bash"
   user "root"
   cwd "/tmp/"
   code <<-EOH
   sudo apt-get -y --no-install-recommends install xserver-xorg xinit openbox xterm
+
+  sudo apt-get install cairo-dock
+
+  mkdir -p ~/.config/openbox
+  cp -R /etc/xdg/openbox/* ~/.config/openbox
+  cp /home/vagrant/menu.xml ~/.config/openbox
+  rm /home/vagrant/menu.xml
   EOH
 end
